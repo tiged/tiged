e9ceb6ac95c7c4a79cc6bf345fb98bd42c15357e
Arya Emami <aryaemami59@yahoo.com>
2024-02-29 10:10:04 -0600

    Convert some mocha syntax to Vitest

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,42 +1,22 @@
-require('source-map-support').install();
+import fs from 'fs/promises';
+import assert from 'node:assert';
+import child_process from 'node:child_process';
+import path from 'node:path';
+import { promisify } from 'node:util';
+import { rimraf } from 'rimraf';
+import glob from 'tiny-glob/sync';
+const exec = promisify(child_process.exec);
 
-const fs = require('fs-extra');
-const path = require('path');
-const glob = require('tiny-glob/sync');
-const { rimraf } = require('../src/utils');
-const assert = require('assert');
-const child_process = require('child_process');
+import degit from '../src/index';
+const degitPath = path.join('dist', 'bin.js');
 
-const degit = require('../src/index.js');
-const degitPath = path.resolve('src/bin.js');
+const timeout = 30_000;
 
-const timeout = 30000;
+beforeAll(async () => {
+	await exec('npm run build');
+});
 
-function exec(cmd) {
-	return new Promise((fulfil, reject) => {
-		child_process.exec(cmd, (err, stdout, stderr) => {
-			if (err) return reject(err);
-			console.log(stdout);
-			console.error(stderr);
-			fulfil();
-		});
-	});
-}
-
-describe('degit', function () {
-	this.timeout(timeout);
-
-	function compare(dir, files) {
-		const expected = glob('**', { cwd: dir });
-		assert.deepStrictEqual(Object.keys(files).sort(), expected.sort());
-
-		expected.forEach(file => {
-			if (!fs.lstatSync(`${dir}/${file}`).isDirectory()) {
-				assert.strictEqual(files[file].trim(), read(`${dir}/${file}`).trim());
-			}
-		});
-	}
-
+describe.sequential('degit', { timeout }, () => {
 	beforeEach(async () => {
 		await rimraf('.tmp');
 	});
@@ -44,140 +24,140 @@ describe('degit', function () {
 		await rimraf('.tmp');
 	});
 
+	function compare<T extends Record<string, any>>(dir: string, files: T) {
+		const expected = glob('**', { cwd: dir });
+		assert.deepStrictEqual(Object.keys(files).sort(), expected.sort());
+
+		expected.forEach(async file => {
+			if (!(await fs.stat(path.join(`${dir}/${file}`))).isDirectory()) {
+				assert.strictEqual(
+					path.join(files[file]).trim(),
+					(await read(path.join(`${dir}/${file}`))).trim()
+				);
+			}
+		});
+	}
+
 	describe('github', () => {
-		[
+		it.each([
 			'tiged/tiged-test-repo-compose',
 			'tiged/tiged-test-repo',
 			'github:tiged/tiged-test-repo',
 			'git@github.com:tiged/tiged-test-repo',
 			'https://github.com/tiged/tiged-test-repo.git'
-		].forEach(src => {
-			it(src, async () => {
-				await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
-				compare(`.tmp/test-repo`, {
-					'file.txt': 'hello from github!',
-					subdir: null,
-					'subdir/file.txt': 'hello from a subdirectory!'
-				});
+		])('%s', async src => {
+			await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
+			compare(`.tmp/test-repo`, {
+				'file.txt': 'hello from github!',
+				subdir: null,
+				[path.join('subdir/file.txt')]: 'hello from a subdirectory!'
 			});
 		});
 	});
 
 	describe('gitlab', () => {
-		[
+		it.each([
 			'gitlab:nake89/tiged-test-repo',
 			'git@gitlab.com:nake89/tiged-test-repo',
 			'https://gitlab.com/nake89/tiged-test-repo.git'
-		].forEach(src => {
-			it(src, async () => {
-				await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
-				compare(`.tmp/test-repo`, {
-					'file.txt': 'hello from gitlab!'
-				});
+		])('%s', async src => {
+			await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
+			compare(path.join(`.tmp/test-repo`), {
+				'file.txt': 'hello from gitlab!'
 			});
 		});
 	});
 
 	describe('gitlab subgroup', () => {
-		['https://gitlab.com/group-test-repo/subgroup-test-repo/test-repo'].forEach(
-			src => {
-				it(src, async () => {
-					await exec(`node ${degitPath} --subgroup ${src} .tmp/test-repo -v`);
-					compare(`.tmp/test-repo`, {
-						'main.tf': 'Subgroup test',
-						subdir1: null,
-						'subdir1/subdir2': null,
-						'subdir1/subdir2/file.txt': "I'm a file."
-					});
-				});
-			}
-		);
+		it.each([
+			'https://gitlab.com/group-test-repo/subgroup-test-repo/test-repo'
+		])('%s', async src => {
+			await exec(`node ${degitPath} --subgroup ${src} .tmp/test-repo -v`);
+			compare(`.tmp/test-repo`, {
+				'main.tf': 'Subgroup test',
+				subdir1: null,
+				[path.join('subdir1/subdir2')]: null,
+				[path.join('subdir1/subdir2/file.txt')]: "I'm a file."
+			});
+		});
 	});
 
 	describe('gitlab subgroup with subdir', () => {
-		['https://gitlab.com/group-test-repo/subgroup-test-repo/test-repo'].forEach(
-			src => {
-				it(src, async () => {
-					await exec(
-						`node ${degitPath} --subgroup ${src} --sub-directory subdir1 .tmp/test-repo -v`
-					);
-					compare(`.tmp/test-repo`, {
-						subdir2: null,
-						'subdir2/file.txt': "I'm a file."
-					});
-				});
+		it.each([
+			'https://gitlab.com/group-test-repo/subgroup-test-repo/test-repo'
+		])('%s', async src => {
+			await exec(
+				`node ${degitPath} --subgroup ${src} --sub-directory subdir1 .tmp/test-repo -v`
+			);
+			compare(`.tmp/test-repo`, {
+				subdir2: null,
+				[path.join('subdir2/file.txt')]: "I'm a file."
+			});
+		});
 
-				it(src, async () => {
-					await exec(
-						`node ${degitPath} --subgroup ${src} --sub-directory subdir1/subdir2 .tmp/test-repo -v`
-					);
-					compare(`.tmp/test-repo`, {
-						'file.txt': "I'm a file."
-					});
-				});
-			}
-		);
+		it.each([
+			'https://gitlab.com/group-test-repo/subgroup-test-repo/test-repo'
+		])('%s', async src => {
+			await exec(
+				`node ${degitPath} --subgroup ${src} --sub-directory subdir1/subdir2 .tmp/test-repo -v`
+			);
+			compare(`.tmp/test-repo`, {
+				'file.txt': "I'm a file."
+			});
+		});
 	});
 
 	describe('bitbucket', () => {
-		[
+		it.each([
 			'bitbucket:nake89/tiged-test-repo',
 			'git@bitbucket.org:nake89/tiged-test-repo',
 			'https://bitbucket.org/nake89/tiged-test-repo.git'
-		].forEach(src => {
-			it(src, async () => {
-				await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
-				compare(`.tmp/test-repo`, {
-					'file.txt': 'hello from bitbucket'
-				});
+		])('%s', async src => {
+			await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
+			compare(`.tmp/test-repo`, {
+				'file.txt': 'hello from bitbucket'
 			});
 		});
 	});
 
 	describe('Sourcehut', () => {
-		[
+		it.each([
 			'git.sr.ht/~satotake/degit-test-repo',
 			'https://git.sr.ht/~satotake/degit-test-repo',
 			'git@git.sr.ht:~satotake/degit-test-repo'
-		].forEach(src => {
-			it(src, async () => {
-				await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
-				compare(`.tmp/test-repo`, {
-					'file.txt': 'hello from sourcehut!'
-				});
+		])('%s', async src => {
+			await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
+			compare(`.tmp/test-repo`, {
+				'file.txt': 'hello from sourcehut!'
 			});
 		});
 	});
 
 	describe('Hugging Face', () => {
-		[
+		it.each([
 			'huggingface:severo/degit-test-repo',
 			'git@huggingface.co:severo/degit-test-repo',
 			'https://huggingface.co/severo/degit-test-repo.git'
-		].forEach(src => {
-			it(src, async () => {
-				await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
-				compare(`.tmp/test-repo`, {
-					'file.txt': 'hello from Hugging Face',
-					subdir: null,
-					'subdir/file.txt': 'hello from a subdirectory!'
-				});
+		])('%s', async src => {
+			await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
+			compare(`.tmp/test-repo`, {
+				'file.txt': 'hello from Hugging Face',
+				subdir: null,
+				[path.join('subdir/file.txt')]: 'hello from a subdirectory!'
 			});
 		});
 	});
 
 	describe('Subdirectories', () => {
-		[
+		it.each([
 			'tiged/tiged-test-repo/subdir',
 			'github:tiged/tiged-test-repo/subdir',
 			'git@github.com:tiged/tiged-test-repo/subdir',
 			'https://github.com/tiged/tiged-test-repo.git/subdir'
-		].forEach(src => {
-			it(src, async () => {
-				await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
-				compare(`.tmp/test-repo`, {
-					'file.txt': 'hello from a subdirectory!'
-				});
+		])('%s', async src => {
+			await exec(`node ${degitPath} ${src} .tmp/test-repo -v`);
+			compare(`.tmp/test-repo`, {
+				'file.txt': 'hello from a subdirectory!'
 			});
 		});
 	});
@@ -187,35 +167,31 @@ describe('degit', function () {
 			let succeeded;
 
 			try {
-				await exec(`mkdir -p .tmp/test-repo`);
+				await fs.mkdir(path.join('.tmp/test-repo'), { recursive: true });
 				await exec(`echo "not empty" > .tmp/test-repo/file.txt`);
-				await exec(
-					`node ${degitPath} tiged/tiged-test-repo .tmp/test-repo -v`
-				);
+				await exec(`node ${degitPath} tiged/tiged-test-repo .tmp/test-repo -v`);
 				succeeded = true;
 			} catch (err) {
-				assert.ok(/destination directory is not empty/.test(err.message));
+				assert.ok(
+					/destination directory is not empty/.test((err as Error).message)
+				);
 			}
 
 			assert.ok(!succeeded);
 		});
 
 		it('succeeds with --force', async () => {
-			await exec(
-				`node ${degitPath} tiged/tiged-test-repo .tmp/test-repo -fv`
-			);
+			await exec(`node ${degitPath} tiged/tiged-test-repo .tmp/test-repo -fv`);
 		});
 	});
 
 	describe('command line arguments', () => {
 		it('allows flags wherever', async () => {
-			await exec(
-				`node ${degitPath} -v tiged/tiged-test-repo .tmp/test-repo`
-			);
+			await exec(`node ${degitPath} -v tiged/tiged-test-repo .tmp/test-repo`);
 			compare(`.tmp/test-repo`, {
 				'file.txt': 'hello from github!',
 				subdir: null,
-				'subdir/file.txt': 'hello from a subdirectory!'
+				[path.join('subdir/file.txt')]: 'hello from a subdirectory!'
 			});
 		});
 	});
@@ -229,7 +205,7 @@ describe('degit', function () {
 			compare(`.tmp/test-repo`, {
 				'file.txt': 'hello from github!',
 				subdir: null,
-				'subdir/file.txt': 'hello from a subdirectory!'
+				[path.join('subdir/file.txt')]: 'hello from a subdirectory!'
 			});
 		});
 	});
@@ -249,13 +225,11 @@ describe('degit', function () {
 			compare(`.tmp/test-repo`, {
 				'other.txt': 'hello from github!',
 				subdir: null,
-				'subdir/file.txt': 'hello from a subdirectory!'
+				[path.join('subdir/file.txt')]: 'hello from a subdirectory!'
 			});
 		});
 
 		it('removes and adds nested files', async () => {
-			await rimraf('.tmp');
-
 			await exec(
 				`node ${degitPath} -v tiged/tiged-test-repo-nested-actions .tmp/test-repo`
 			);
@@ -263,32 +237,30 @@ describe('degit', function () {
 				dir: null,
 				folder: null,
 				subdir: null,
-				'folder/file.txt': 'hello from clobber file!',
-				'folder/other.txt': 'hello from other file!',
-				'subdir/file.txt': 'hello from a subdirectory!'
+				[path.join('folder/file.txt')]: 'hello from clobber file!',
+				[path.join('folder/other.txt')]: 'hello from other file!',
+				[path.join('subdir/file.txt')]: 'hello from a subdirectory!'
 			});
 		});
 	});
 
 	describe('git mode old hash', () => {
 		it('is able to clone correctly using git mode with old hash', async () => {
-			await rimraf('.tmp');
 			await exec(
-				`node ${degitPath} --mode=git https://github.com/tiged/tiged-test.git#b09755bc4cca3d3b398fbe5e411daeae79869581 .tmp/test-repo`
+				`node ${degitPath} --mode=git https://github.com/tiged/tiged-test.git#525e8fef2c6b5e261511adc55f410d83ca5d8256 .tmp/test-repo`
 			);
 			compare(`.tmp/test-repo`, {
 				subdir: false,
 				'README.md': 'tiged is awesome',
-				'subdir/file': 'Hello, champ!'
+				[path.join('subdir/file')]: 'Hello, buddy!'
 			});
 		});
 		it('is able to clone subdir correctly using git mode with old hash', async () => {
-			await rimraf('.tmp');
 			await exec(
 				`node ${degitPath} --mode=git https://github.com/tiged/tiged-test.git/subdir#b09755bc4cca3d3b398fbe5e411daeae79869581 .tmp/test-repo`
 			);
 			compare(`.tmp/test-repo`, {
-				file: 'Hello, champ!'
+				file: 'Hello, buddy!'
 			});
 		});
 	});
@@ -306,6 +278,6 @@ describe('degit', function () {
 	});
 });
 
-function read(file) {
-	return fs.readFileSync(file, 'utf-8');
+async function read(file: string) {
+	return await fs.readFile(file, 'utf-8');
 }
