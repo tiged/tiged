805fb99e28bc12033c241bb5c28a5f72add321ab
Arya Emami <aryaemami59@yahoo.com>
2024-03-26 11:08:06 -0500

    Fix import of `tiny-glob/sync`

--- src/bin.ts
+++ src/bin.ts
@@ -6,7 +6,7 @@ import fuzzysearch from 'fuzzysearch';
 import mri from 'mri';
 import fs from 'node:fs';
 import path from 'node:path';
-import glob from 'tiny-glob/sync.js';
+import glob from 'tiny-glob/sync';
 import type { Options } from './index';
 import degit from './index';
 import { base, tryRequire } from './utils';
